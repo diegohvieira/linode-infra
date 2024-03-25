324cd3b7852800b28d873dc8e6c1b4b5b1c988c3
diegohvieira <diegohvcarlos@gmail.com>
2024-03-25 20:35:05 -0300

    feat: terraform workflow

--- beckend.tf
+++ beckend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-us-east-1-0423"
     use_path_style = true
     key            = "infra/terraform.tfstate"
     region         = "us-east-1"
@@ -12,29 +11,5 @@ terraform {
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_s3_checksum            = true
-    access_key                  = "D8X9WFP5YUGL432GEMNE"
-    secret_key                  = "CngeGOLhZR9P8eCxHhtamq9dKVLcnFbHU5ToS8PV"
-    #workspace_key_prefix        = ""
   }
 }
-
-
-#terraform {
-#  backend "s3" {
-#    skip_credentials_validation = true
-#    skip_requesting_account_id  = true
-#    skip_metadata_api_check     = true
-#    skip_region_validation      = true
-#    #skip_s3_checksum            = true
-#    use_path_style = true
-#    endpoints = {
-#      s3 = "https://us-east-1.linodeobjects.com"
-#    }
-#    bucket = "terraform-us-east-1-0423"
-#    key    = "infra/terraform.tfstate"
-#    region = "us-east-1"
-#    #access_key = "D8X9WFP5YUGL432GEMNE"
-#    #secret_key = "CngeGOLhZR9P8eCxHhtamq9dKVLcnFbHU5ToS8PV"
-#  }
-#}
-#
