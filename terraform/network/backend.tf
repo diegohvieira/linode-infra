8b8fed190cca3b81b4a66975def6c2a6cc14c6ba
diegohvieira <diegohvcarlos@gmail.com>
2024-06-04 16:29:51 -0300

    feat: onboarding tests

--- terraform/network/backend.tf
+++ terraform/network/backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     use_path_style = true
     key            = "network/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-mia-1"
     endpoints = {
-      s3 = "https://us-east-1.linodeobjects.com"
+      s3 = "https://us-mia-1.linodeobjects.com"
     }
     skip_credentials_validation = true
     skip_requesting_account_id  = true
